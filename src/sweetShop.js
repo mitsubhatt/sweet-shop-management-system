1e11d1a8679466030843de2bb2e8cc7880343eea
mitsubhatt <mitscode1204@gmail.com>
2025-07-16 16:40:04 +0530

    added deleteSweet method with test and erroring for non-existent sweet

--- src/sweetShop.js
+++ src/sweetShop.js
@@ -26,6 +26,12 @@ class SweetShop {
     viewSweets() {
         return this.sweets;
     }
+
+    deleteSweet(id) {
+        const index = this.sweets.findIndex(s => s.id == id);
+        if(index == -1) throw new Error('Sweet not found');
+        this.sweets.splice(index,1);
+    }
 }
 
 module.exports = SweetShop;
--- tests/sweetShop.test.js
+++ tests/sweetShop.test.js
@@ -1,7 +1,21 @@
 const SweetShop = require('../src/sweetShop');
 
-test('Throws an error when adding a sweet with a duplicate id', () => {
-  
+test('Add a sweet to the shop', () => {
+  const shop = new SweetShop();
+  shop.addSweet({
+    id: 1,
+    name: 'Kaju Katli',
+    category: 'Nut-Based',
+    price: 50,
+    quantity: 20
+  });
+
+  const sweets = shop.viewSweets();
+  expect(sweets.length).toBe(1);
+  expect(sweets[0].name).toBe('Kaju Katli');
+});
+
+test('Throws error when adding a sweet with a duplicate id', () => {
     const shop = new SweetShop();
 
     shop.addSweet({id:1, name:'Laddoo',category:'Traditional',price:50, quantity:20});
@@ -10,8 +24,7 @@ test('Throws an error when adding a sweet with a duplicate id', () => {
     }).toThrow('Sweet with this id already exists!');
 });
 
-test('Throws an error for missing fields', () => {
-  
+test('Throws error for missing fields', () => {
     const shop = new SweetShop();
 
     expect( () => {
@@ -20,11 +33,28 @@ test('Throws an error for missing fields', () => {
 });
 
 test('Throws error when adding a sweet with a duplicate id ', () => {
-  
     const shop = new SweetShop();
 
     expect( () => {
         shop.addSweet({id:3, name:'Barfi',category:'Milk-based',price:-50, quantity:15});
     }).toThrow('Price and quantity must be non-negative');
 });
+
+test('Delete a sweet from the shop', () => {
+    const shop = new SweetShop();
+    shop.addSweet({id:1, name:'Gulab Jamun',category:'Milk-based',price:50, quantity:15});
+
+    shop.deleteSweet(1);
+    const sweets = shop.viewSweets();
+    expect(sweets.length).toBe(0);
+});
+
+test('Throws error when trying to delete a non-existent sweet', () => {
+  const shop = new SweetShop();
+
+  expect( () => {
+    shop.deleteSweet(99);
+  }).toThrow('Sweet not found');
+});
+
 
