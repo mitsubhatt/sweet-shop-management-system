1b27d57293878d27ed07474a1b692c85a14dd32b
mitsubhatt <mitscode1204@gmail.com>
2025-07-16 16:20:16 +0530

    added edge-case validation to addSweet (duplicate id, missiong field, negatives)

--- src/sweetShop.js
+++ src/sweetShop.js
@@ -4,6 +4,22 @@ class SweetShop {
     }
 
     addSweet(sweet) {
+        
+        const requiredFields = ['id','name','category','price','quantity'];
+
+        for(let field of requiredFields){
+            if(!(field in sweet)){
+                throw new Error(`Missing required field: ${field}`);
+            }
+        }
+
+        if(this.sweets.some(s => s.id == sweet.id)){
+            throw new Error('Sweet with this id already exists!');
+        }
+
+        if(sweet.price<0 || sweet.quantity<0){
+            throw new Error('Price and quantity must be non-negative');
+        }
         this.sweets.push(sweet);
     }
 
--- tests/sweetShop.test.js
+++ tests/sweetShop.test.js
@@ -1,17 +1,30 @@
 const SweetShop = require('../src/sweetShop');
 
-test('Add a sweet to the shop ', () => {
+test('Throws an error when adding a sweet with a duplicate id', () => {
   
     const shop = new SweetShop();
-    shop.addSweet({
-        id:1,
-        name: 'Kaju Katli',
-        category: 'Nut-based',
-        price:50,
-        quantity:20
-    });
 
-    const sweets = shop.viewSweets();
-    expect(sweets.length).toBe(1);
-    expect(sweets[0].name).toBe('Kaju Katli');
+    shop.addSweet({id:1, name:'Laddoo',category:'Traditional',price:50, quantity:20});
+    expect( () => {
+        shop.addSweet({id:1, name:'Sonpapdi',category:'Traditional',price:30, quantity:5});
+    }).toThrow('Sweet with this id already exists!');
 });
+
+test('Throws an error for missing fields', () => {
+  
+    const shop = new SweetShop();
+
+    expect( () => {
+        shop.addSweet({id:2, name:'Rasgulla'});
+    }).toThrow('Missing required field: category');
+});
+
+test('Throws error when adding a sweet with a duplicate id ', () => {
+  
+    const shop = new SweetShop();
+
+    expect( () => {
+        shop.addSweet({id:3, name:'Barfi',category:'Milk-based',price:-50, quantity:15});
+    }).toThrow('Price and quantity must be non-negative');
+});
+
